3fd8ae54d2d46b6b632650f7f0601bbb112a4384
selfstudyhans <selfstudyhansraj@gmail.com>
2024-07-09 16:24:53 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -133,7 +133,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                 progress=progress_for_pyrogram,
                 progress_args=(
                     client,
-                    "**DOWNLOADING: __Team SPY__**\n ",
+                    "**DOWNLOADING: __Team @hacker_babu __**\n ",
                     edit,
                     time.time()
                 )
@@ -186,7 +186,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     progress=progress_for_pyrogram,
                     progress_args=(
                         client,
-                        '**UPLOADING: __Team SPY__**\n ',
+                        '**UPLOADING: __Team @Hacker_babu __**\n ',
                         upm,
                         time.time()
                     )
@@ -229,7 +229,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     progress=progress_for_pyrogram,
                     progress_args=(
                         client,
-                        '**UPLOADING:**\n**bot made by Mr. Invisible**',
+                        '**UPLOADING:**\n**bot made by Mr. Hacker Babu**',
                         upm,
                         time.time()
                     )
